7402f2dc772d2cd8f3bbfc721578fb75d7730b72
Hripsime Grigoryan <hrigrigo@c1r2s3.42yerevan.am>
2024-02-09 21:34:19 +0400

    aaa

--- get_next_line.c
+++ get_next_line.c
@@ -3,6 +3,37 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_list(t_list *lst)
+{
+	while (lst)
+	{
+		printf("list item: %s\n", lst -> content);
+		lst = lst -> next;
+	}
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		s1_size;
+	char	*s2;
+	int		i;
+
+	i = 0;
+	s1_size = 0;
+	while (s1[s1_size])
+		s1_size++;
+	s2 = (char *)malloc(s1_size * sizeof(char) + 1);
+	if (!s2)
+		return (NULL);
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
 void add_to_list(t_list **lst, char *str)
 {
 	t_list *node;
@@ -38,151 +69,90 @@ int look_for_newline(char *str)
 		}
 		i++;
 	}
-	return (0);
+	return (i);
 }
 
-void print_list(t_list *lst)
-{
-	while (lst)
-	{
-		printf("list item: %s\n", lst -> content);
-		lst = lst -> next;
-	}
-}
-
-void create_list(t_list **lst, int fd)
+int create_list(t_list **lst, int fd)
 {
 	char *str;
 	int bytes_read;
+	int lst_size;
 
+	lst_size = 0;
 	bytes_read = BUFFER_SIZE;
 	str = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!str)
-		return ;
+		return (0);
 	while (bytes_read == BUFFER_SIZE)
 	{
 		bytes_read = read(fd, str, BUFFER_SIZE);
 		if (bytes_read <= 0)
 		{	
 			free(str);
-			return ;
+			return (lst_size);
 		}
 		str[bytes_read] = '\0';
-		add_to_list(lst, strdup(str));
-		if (look_for_newline(str))
-			return ;
+		add_to_list(lst, ft_strdup(str));
+		lst_size += look_for_newline(str);
+		if (look_for_newline(str) != BUFFER_SIZE)
+			break ;
 	}
-	return ;
+	free(str);
+	return (lst_size);
 }
 
-int str_len(t_list *lst)
-{
-	int len;
-	int i;
-
-	len = 0;
-	while (lst && lst -> content)
-	{
-		i = 0;
-		while (lst -> content[i])
-		{
-			if (lst -> content[i] == '\n')
-			{
-				len++;
-				return (len);
-			}
-			i++;
-			len++;
-		}
-		lst = lst -> next;
-	}
-	return (len);
-}
-
-char *create_line(t_list *lst)
-{
-	int len;
-	char *line;	
-	int i;
-	int j;
-
-	if (!lst)
-		return (0);
-	j = 0;
-	len = str_len(lst);
-	line = malloc((len + 1) * sizeof(char));
-	if (!line)
-		return (NULL);
-	while (lst && lst ->content)
-	{
-		i = 0;
-		while (lst ->content[i])
-		{
-			line[j] = lst -> content[i];
-			j++;
-			if (lst -> content[i] == '\n')
-				break ;
-  			i++;
-		}
-		lst = lst -> next;
-	}
-	line[j] = '\0';
-	return (line);
-}
-
-char *get_tail(t_list *lst)
+char *create_line(t_list *lst, int line_len)
 {
 	char *str;
-	int i = 0;
+	int i;
 	int j;
 
 	if (!lst)
 		return (0);
-	while (lst -> next)
-		lst = lst -> next;
-	j = 0;
-	while (lst -> content && lst -> content[j] && lst -> content[j] != '\n')
-		j++;
-	str = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	i = 0;
+	str = malloc((line_len + 1) * sizeof(char));
 	if (!str)
 		return (0);
-	while (lst -> content && lst -> content[j])
+	while (i < line_len && lst)
 	{
-		str[i] = lst -> content[j];
-		i++;
-		j++;
+		j = 0;
+		while (i < line_len && lst -> content[j])
+		{
+			str[i] = lst -> content[j];
+			i++;
+			j++;
+		}
+		lst = lst -> next;
 	}
 	str[i] = '\0';
 	return (str);
 }
 
-void clear_list(t_list **lst)
+void clear_list(t_list **lst, int line_len)
 {
-	t_list *new;
 	t_list *tmp;
-	char *str;
 
-	while (*lst)
+	if (!lst)
+		return ;
+	while (line_len >= BUFFER_SIZE && *lst)
 	{
 		tmp = (*lst) -> next;
 		free((*lst) -> content);
 		free(*lst);
-		(*lst) = tmp;
+		*lst = tmp;
+		line_len -= BUFFER_SIZE;
 	}
-	str = get_tail(*lst);
-	if (!str)
-		*lst = NULL;
-	else
+	while (*lst && *((*lst) -> content))
 	{
-		new = malloc(sizeof(t_list));
-		if (!new)
+		if (*((*lst) -> content) != '\n')
 		{
-			free(str);
-			return ;
+			(*lst) -> content++;
 		}
-		new -> content = str;
-		new -> next = NULL;
-		*lst = new;
+		else
+		{
+			(*lst) -> content++;
+			break ;
+		}
 	}
 }
 
@@ -190,14 +160,15 @@ char *get_next_line(int fd)
 {
 	char *next_line;
 	static t_list *lst = NULL;
+	static int line_len = 0;
 
 	next_line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, next_line, 0) < 0)
 		return (NULL);
-	create_list(&lst, fd);
-	//print_list(lst);
-	next_line = create_line(lst);
-	clear_list(&lst);
+	line_len += create_list(&lst, fd);
+	next_line = create_line(lst, line_len);
+	clear_list(&lst, line_len);
+	line_len = BUFFER_SIZE - (line_len % BUFFER_SIZE);
 	return (next_line);
 }
 
@@ -207,7 +178,7 @@ int main()
 	char *str = get_next_line(fd);
  	printf("%s", str);
 	free(str);
-	//str = get_next_line(fd);
-	//printf("%s", str);
-	system("leaks a.out");
+	str = get_next_line(fd);
+	printf("%s", str);
+	//system("leaks a.out");
 }
---
+++ get_next_line6.c
@@ -0,0 +1,211 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+
+void add_to_list(t_list **lst, char *str)
+{
+	t_list *node;
+	t_list *new;
+	
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return ;
+	new -> content = str;
+	new -> next = NULL;	
+	node = *lst;
+	if (!node)
+	{
+		*lst = new;
+		return ;
+	}
+	while (node -> next)
+		node = node -> next;
+	node -> next = new;
+}
+
+int look_for_newline(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+		{
+			i++;
+			return (i);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void print_list(t_list *lst)
+{
+	while (lst)
+	{
+		printf("list item: %s\n", lst -> content);
+		lst = lst -> next;
+	}
+}
+
+void create_list(t_list **lst, int fd)
+{
+	char *str;
+	int bytes_read;
+
+	bytes_read = BUFFER_SIZE;
+	str = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!str)
+		return ;
+	while (bytes_read == BUFFER_SIZE && !look_for_newline(str))
+	{
+		bytes_read = read(fd, str, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{	
+			free(str);
+			return ;
+		}
+		str[bytes_read] = '\0';
+		add_to_list(lst, strdup(str));
+	}
+	return ;
+}
+
+int str_len(t_list *lst)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (lst && lst -> content)
+	{
+		i = 0;
+		while (lst -> content[i])
+		{
+			if (lst -> content[i] == '\n')
+			{
+				len++;
+				return (len);
+			}
+			i++;
+			len++;
+		}
+		lst = lst -> next;
+	}
+	return (len);
+}
+
+char *create_line(t_list *lst)
+{
+	int len;
+	char *line;	
+	int i;
+	int j;
+
+	if (!lst)
+		return (0);
+	j = 0;
+	len = str_len(lst);
+	line = malloc((len + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	while (lst && lst ->content)
+	{
+		i = 0;
+		while (lst ->content[i])
+		{
+			line[j] = lst -> content[i];
+			j++;
+			if (lst -> content[i] == '\n')
+				break ;
+  			i++;
+		}
+		lst = lst -> next;
+	}
+	line[j] = '\0';
+	return (line);
+}
+
+char *get_tail(t_list *lst)
+{
+	char *str;
+	int i = 0;
+	int j;
+
+	if (!lst)
+		return (0);
+	while (lst -> next)
+		lst = lst -> next;
+	j = 0;
+	while (lst -> content && lst -> content[j] && lst -> content[j] != '\n')
+		j++;
+	str = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	while (lst -> content && lst -> content[j])
+	{
+		str[i] = lst -> content[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+void clear_list(t_list **lst)
+{
+	t_list *new;
+	t_list *tmp;
+	char *str;
+
+	while (*lst)
+	{
+		tmp = (*lst) -> next;
+		free((*lst) -> content);
+		free(*lst);
+		(*lst) = tmp;
+	}
+	str = get_tail(*lst);
+	if (!str)
+		*lst = NULL;
+	else
+	{
+		new = malloc(sizeof(t_list));
+		if (!new)
+		{
+			free(str);
+			return ;
+		}
+		new -> content = str;
+		new -> next = NULL;
+		*lst = new;
+	}
+}
+
+char *get_next_line(int fd)
+{
+	char *next_line;
+	static t_list *lst = NULL;
+
+	next_line = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, next_line, 0) < 0)
+		return (NULL);
+	create_list(&lst, fd);
+	//print_list(lst);
+	next_line = create_line(lst);
+	clear_list(&lst);
+	return (next_line);
+}
+
+int main()
+{
+	int fd = open("gnlTester/files/nl", O_RDWR);
+	char *str = get_next_line(fd);
+ 	printf("%s", str);
+	free(str);
+	//str = get_next_line(fd);
+	//printf("%s", str);
+	system("leaks a.out");
+}
